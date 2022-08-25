983bc10d5c23a295cf7c45ede57e824e48859994
Providence NDRI <thierryprovidence@gmail.com>
2022-08-25 20:07:08 +0100

    widgets

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
 import "./app.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,69 +1,25 @@
-import './chart.css'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import './chart.css';
+import { LineChart, 
+         Line, 
+         XAxis, 
+         CartesianGrid, 
+         Tooltip, 
+         ResponsiveContainer } from 'recharts';
 
 
-export default function Chart() {
-    const data = [
-        {
-          name: 'Jan',
-          "Active user": 4000,
-        },
-        {
-          name: 'Feb',
-          "Active user": 3000,
-        },
-        {
-          name: 'Mar',
-          "Active user": 2000,
-        },
-        {
-          name: 'Apr',
-          "Active user": 2780,
-        },
-        {
-          name: 'May',
-          "Active user": 1890,
-        },
-        {
-          name: 'Jun',
-          "Active user": 2340,
-        },
-        {
-          name: 'Jul',
-          "Active user": 2490,
-        },
-        {
-            name: 'Agu',
-            "Active user": 3410,
-        },
-        {
-            name: 'Sep',
-            "Active user": 1492,
-        },
-        {
-            name: 'Oct',
-            "Active user": 3950,
-        },
-        {
-            name: 'Nov',
-            "Active user": 5510,
-        },
-        {
-            name: 'Dec',
-            "Active user": 2140,
-        }
-      ];
-      
+export default function Chart({ title, data, dataKey, grid }) {
+          
     return (
         <div className='chart'>
-            <h3 className="chartTitle">User Analytics</h3>
+            <h3 className="chartTitle">{title}</h3>
             <ResponsiveContainer width="100%" aspect={4 / 1}>
                 <LineChart data={data}>
                     <XAxis dataKey="name" stroke="#5550bd" />
-                    <Line type="monotone" dataKey="Active user" stroke="#5550bd" />
+                    <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+                    <Tooltip />
+                    {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray="5 5" />}
                 </LineChart>
-
             </ResponsiveContainer>
         </div>
-    )
+    );
 }
